eb9142863902a092e1b2f61da00bd16b3102ebf5
jxxh204 <ahhancom@gmail.com>
2023-12-29 17:43:57 +0900

    FEAT : Styled를 이용해서
    Task.tsx : Hover 기능 작성

--- src/component/Task/index.tsx
+++ src/component/Task/index.tsx
@@ -1,22 +1,38 @@
 import styled from "styled-components";
-import Submit from "./Submit";
 import Input from "./Input";
+import Submit from "./Submit";
+import { StyleHover } from "../Hover";
 
-const StyleForm = styled.form`
+type Props = {
+  children: React.ReactNode;
+  focus: boolean;
+  handler: React.FormEventHandler<HTMLFormElement>;
+};
+type StyleFromProps = {
+  focusMode: boolean;
+};
+const StyleForm = styled.form<StyleFromProps>`
   display: flex;
   flex-direction: row;
   /* justify-content: center; */
+
   gap: 10px;
+  &:hover {
+    ${StyleHover} {
+      display: ${(props) => (props.focusMode ? "block" : "none")};
+    }
+  }
 `;
-type Props = {
-  children: React.ReactNode;
-  handler: React.FormEventHandler<HTMLFormElement>;
-};
-function Form({ children, handler }: Props) {
-  return <StyleForm onSubmit={handler}>{children}</StyleForm>;
+
+function Task({ children, focus, handler }: Props) {
+  return (
+    <StyleForm focusMode={focus} onSubmit={handler}>
+      {children}
+    </StyleForm>
+  );
 }
 
-Form.Input = Input;
-Form.Submit = Submit;
+Task.Input = Input;
+Task.Submit = Submit;
 
-export default Form;
+export default Task;
