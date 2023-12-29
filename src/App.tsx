7af5841c6ecc483cbb3a5481be9661ba0472f86e
jxxh204 <ahhancom@gmail.com>
2023-12-29 17:03:23 +0900

    CHORE : 네이밍 변경
    Form => Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import useTask from "./hooks/useTask";
-import Form from "./component/Form";
+import Task from "./component/Task";
 
 function App() {
   const { text, isFocus, textHandler, handleSubmit } = useTask();
   return (
     <div className="App">
-      <Form handler={handleSubmit}>
-        <Form.Input text={text} focus={isFocus} handleChange={textHandler} />
-        <Form.Submit focus={isFocus} />
-      </Form>
+      <Task handler={handleSubmit}>
+        <Hover>
+          <Hover.Edit />
+          <Hover.Delete />
+        </Hover>
+        <Task.Input text={text} focus={isFocus} handleChange={textHandler} />
+        <Task.Submit focus={isFocus} />
+      </Task>
     </div>
   );
 }
