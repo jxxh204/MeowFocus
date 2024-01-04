8ec8ed031950a548e0f0c2285e4fe4a8dda03c7b
jxxh204 <ahhancom@gmail.com>
2024-01-04 18:36:14 +0900

    TEST : 디자인된 UI 테스트 작성중 Task Input을 입력하기 위해 클릭했을 경우. 1. 배경색상 변경 2. 저장 버튼 생성 3. 글자 수 표현, 50글자까지 구현

--- src/test/Task.test.tsx
+++ src/test/Task.test.tsx
@@ -18,96 +18,107 @@ const setup = () => {
   return { ...utils };
 };
 
-describe("Task Input", () => {
-  test("태스크를 입력하면 텍스트는 유지하고 버튼은 사라집니다.", async () => {
+describe("Task Input을 입력하기위해 클릭", () => {
+  beforeEach(() => {
     setup();
-
-    const inputText =
-      screen.getByPlaceholderText("지금 집중할 일을 적어주세요.");
-    const submitButton = screen.getByText("입력");
-
-    userEvent.type(inputText, "면접 공부");
-    expect(inputText).toHaveValue("면접 공부");
-
-    await userEvent.click(submitButton);
-
-    //결과
-    expect(inputText).toHaveValue("면접 공부");
-    expect(inputText).toBeDisabled();
-    expect(submitButton).not.toBeVisible();
   });
+  test("배경색상 변경", async () => {
+    const taskBackground = screen.getByTestId("background-color");
+    const inputText = screen.getByPlaceholderText(
+      "집중이 필요한 일 한가지를 적어주세요."
+    );
+    expect(taskBackground).toHaveStyle("fill:100%");
+    await userEvent.click(inputText);
+    expect(taskBackground).toHaveStyle("fill:80%");
+    // const submitButton = screen.getByText("입력");
+
+    // userEvent.type(inputText, "면접 공부");
+    // expect(inputText).toHaveValue("면접 공부");
+
+    // await userEvent.click(submitButton);
+
+    // //결과
+    // expect(inputText).toHaveValue("면접 공부");
+    // expect(inputText).toBeDisabled();
+    // expect(submitButton).not.toBeVisible();
+  });
+  test("저장 버튼 생성", async () => {
+    const inputText = screen.getByPlaceholderText(
+      "집중이 필요한 일 한가지를 적어주세요."
+    );
+    await userEvent.click(inputText);
+    const saveButton = screen.getByRole("button", { name: /저장/ });
+    expect(saveButton).toBeDefined();
+  });
+  test("글자 수 표현, 50글자까지 구현", async () => {});
 });
 
-describe("Task Edit", () => {
-  test("포커스 모드에서만 호버가 가능해야 합니다.", async () => {
-    setup();
+// describe("Task Edit", () => {
+//   test("포커스 모드에서만 호버가 가능해야 합니다.", async () => {
+//     setup();
 
-    const inputText =
-      screen.getByPlaceholderText("지금 집중할 일을 적어주세요.");
-    const submitButton = screen.getByText("입력");
+//     const inputText =
+//       screen.getByPlaceholderText("지금 집중할 일을 적어주세요.");
+//     const submitButton = screen.getByText("입력");
 
-    userEvent.type(inputText, "면접 공부");
-    expect(inputText).toHaveValue("면접 공부");
+//     userEvent.type(inputText, "면접 공부");
+//     expect(inputText).toHaveValue("면접 공부");
 
-    await userEvent.click(submitButton);
+//     await userEvent.click(submitButton);
 
-    expect(inputText).toBeDisabled();
+//     expect(inputText).toBeDisabled();
 
-    await userEvent.hover(inputText);
-    // 고치기
-    waitFor(() => {
-      const editButton = screen.getByLabelText(/수정/);
-      expect(editButton).toBeDefined();
-    });
-  });
-});
+//     await userEvent.hover(inputText);
+//     // 고치기
+//     waitFor(() => {
+//       const editButton = screen.getByLabelText(/수정/);
+//       expect(editButton).toBeDefined();
+//     });
+//   });
+// });
 
-describe("Task Delete", () => {
-  const 텍스트입력 = async () => {
-    const inputText =
-      screen.getByPlaceholderText("지금 집중할 일을 적어주세요.");
-    const submitButton = screen.getByText("입력");
+// describe("Task Delete", () => {
+//   const 텍스트입력 = async () => {
+//     const inputText =
+//       screen.getByPlaceholderText("지금 집중할 일을 적어주세요.");
+//     const submitButton = screen.getByText("입력");
 
-    userEvent.type(inputText, "면접 공부");
-    expect(inputText).toHaveValue("면접 공부");
+//     userEvent.type(inputText, "면접 공부");
+//     expect(inputText).toHaveValue("면접 공부");
 
-    await userEvent.click(submitButton);
+//     await userEvent.click(submitButton);
 
-    expect(inputText).toBeDisabled();
+//     expect(inputText).toBeDisabled();
 
-    return { textElement: inputText };
+//     return { textElement: inputText };
 
-    //
-  };
-  test("포커스 모드에서만 호버가 가능해야 합니다.", async () => {
-    setup();
-    const { textElement } = await 텍스트입력();
-    await userEvent.hover(textElement);
+//     //
+//   };
+//   test("포커스 모드에서만 호버가 가능해야 합니다.", async () => {
+//     setup();
+//     const { textElement } = await 텍스트입력();
+//     await userEvent.hover(textElement);
 
-    // 고치기
-    waitFor(() => {
-      const deleteButton = screen.getByRole("button", { name: /삭제/ });
-      expect(deleteButton).toBeDefined();
-    });
-  });
-  test("삭제 버튼을 누릅니다. 텍스트는 초기화되고 포커스모드는 false가 됩니다.", async () => {
-    setup();
-    const { textElement } = await 텍스트입력();
-    await userEvent.hover(textElement);
+//     // 고치기
+//     waitFor(() => {
+//       const deleteButton = screen.getByRole("button", { name: /삭제/ });
+//       expect(deleteButton).toBeDefined();
+//     });
+//   });
+//   test("삭제 버튼을 누릅니다. 텍스트는 초기화되고 포커스모드는 false가 됩니다.", async () => {
+//     setup();
+//     const { textElement } = await 텍스트입력();
+//     await userEvent.hover(textElement);
 
-    waitFor(async () => {
-      // 버튼 확인
-      const deleteButton = screen.getByRole("button", { name: /삭제/ });
-      await userEvent.click(deleteButton);
-      await expect(textElement).toHaveValue("");
-      await expect(textElement).toBeDisabled();
-      const submitButton = screen.getByText("입력");
-      expect(submitButton).toBeDefined();
-    });
-  });
-  test("Task Create", () => {});
-});
-//1. text input 빈공간 placeholder : 지금 집중할 일을 적어주세요.
-//2. 입력완료 - 입력 버튼
-//3. text input 비활성화
-//3. 수정 / 완료 버튼 생성.
+//     waitFor(async () => {
+//       // 버튼 확인
+//       const deleteButton = screen.getByRole("button", { name: /삭제/ });
+//       await userEvent.click(deleteButton);
+//       await expect(textElement).toHaveValue("");
+//       await expect(textElement).toBeDisabled();
+//       const submitButton = screen.getByText("입력");
+//       expect(submitButton).toBeDefined();
+//     });
+//   });
+//   test("Task Create", () => {});
+// });
